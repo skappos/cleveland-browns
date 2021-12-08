286872670c6e491e88b2a4840dedc2a6ec83540c
Sophia Kappos <43252048+skappos@users.noreply.github.com>
2021-12-08 18:08:19 -0500

    Create hofRender.js

---
+++ hofRender.js
@@ -0,0 +1,221 @@
+
+let hallOfFamers =
+[
+  {
+    img: "images/jim brown.png",
+    firstName: "Jim",
+    nickname: "",
+    lastname: "Brown",
+    position: "RB" ,
+    seasons: 9,
+    years: "1957-65",
+    stats: "12,312 rushing yards /n 126 career TDs /n",
+    bio:"'Yardage isn't the big thing. Having your team win the championship is... That's what I work for, winning the championship, and this requres a certain standard of performance.'"
+
+  },
+  {
+    img: "images/paul brown.png",
+    firstName: "Paul",
+    nickname: "",
+    lastname:"Brown",
+    position: "HC" ,
+    seasons: 17,
+    years: "1946-62",
+    stats:"167-53-8 record /n 8 league championships",
+    bio: "'The most pleasant way of life is to have a job you love and I love football coaching. Money is relatively unimportant to me once I've taken care of the comforts of my family.'"
+
+  },
+  {
+    img: "images/bill willis.png",
+    firstName: "Bill",
+    nickname: "",
+    lastname:"Willis",
+    position: "DT" ,
+    seasons: 8,
+    years: "1946-53",
+    bio: "'The split second the ball moved, I charged and I always came at a different angle... I could unleash a pretty good forearm block and a rather devastating tackle too.'"
+
+  },
+  {
+    img: "images/bobbyMitchell.png",
+    firstName: "Bobby ",
+    nickname: "",
+    lastname: "Mitchell",
+    position: "WR" ,
+    seasons: 11,
+    years: "1958-61",
+    stats: "14,078 net yards /n 521 career receptions /n 91 career TDs",
+    bio: "'The defense man, he's at a disadvantage. If he treies to cover me man to man, that is unheard of... I'm not a cocky individual. I'm confident, but you have to go out there confident. If you come hesitating, that defensemen, he has you in his back pocket.'"
+
+  },
+  {
+    img: "images/danteLavelli.png",
+    firstName: "Dante",
+    nickname: "'Glue fingers'",
+    lastname: "Lavelli",
+    position: "WR" ,
+    seasons: 11,
+    years: "1946-56",
+    stats: "6,488 receiving yards /n 386 career receptions /n 62 career TDs",
+    bio: "'They talk about the bump-and-run nowadays. Heck, we had that with one difference. A defender didn't bump you, he knocked you down.'"
+
+  },
+  {
+    img: "images/frankGatski.png",
+    firstName: "Frank",
+    nickname: "'Gunner'",
+    lastname: "Gatski",
+    position: "C" ,
+    seasons: 12,
+    years: "1946-56",
+    bio: "'Just being there. Just winning all those games. That's what it was all about for me.'"
+
+  },
+  {
+    img: "images/geneHickerson.png",
+    firstName: "Gene",
+    nickname: "",
+    lastname: "Hickerson",
+    position: "G" ,
+    seasons: 15,
+    years: "1958-73",
+    bio:"'By taking personal pride in what I do. I want the respect of my teammates.'"
+
+  },
+  {
+    img: "images/JoeDeLamielleure.png",
+    firstName: "Joe",
+    nickname:"'Joe D'",
+    lastname: "DeLamielleure",
+    position: "G" ,
+    seasons: 13,
+    years: "1980-84",
+    bio:"'I just enjoyed it. I like working hard and I liked doing the little things that would make you good. I loved football. I loved playing it.'"
+
+
+  },
+  {
+    img: "images/lenFord.png",
+    firstName: "Len",
+    nickname: "",
+    lastname: "Ford",
+    position: "DE" ,
+    seasons: 11,
+    years: "1950-57",
+    bio:"'The result you get from any endeavor depends upon the effort you put into it.'"
+
+  },
+  {
+    img: "images/leroyKelly.png",
+    firstName: "Leroy",
+    nickname: "",
+    lastname: "Kelly",
+    position: "RB" ,
+    seasons: 10,
+    years: "1964-73",
+    stats:"7,274 rushing yards /n 2,281 receiving yards /n 12,330 combined yards",
+    bio:"'I didn't try to think about replacing Jim Brown. When I was sitting on the bench 2 years, I used to compare myself to the backs that were starting around the leagure, and I knew I was as good as most of them. I just want to be one of the best backs in the league, not a superman like Jim Brown was.'"
+
+  },
+  {
+    img: "images/louGroza.png",
+    firstName: "Lou",
+    nickname: "'The Toe'",
+    lastname: "Groza",
+    position: "OT/K" ,
+    seasons: 21,
+    years: "1946-59, 1961-67",
+    stats:"1,608 career points",
+    bio: "'Kicking was something I did because I had the talent. I always considered myself a tackle.'"
+
+
+  },
+  {
+    img: "images/macSpeedie.png",
+    firstName: "Mac",
+    nickname: "",
+    lastname: "Speedie",
+    position: "E" ,
+    seasons: 7,
+    years: "1946-52",
+    bio:"'Hurdling is all speed, balance, and perfect timing. I think that helped me more than anything else in becoming successful in football.'"
+
+  },
+  {
+    img: "images/marionMotley.png",
+    firstName: "Marion",
+    nickname: "",
+    lastname: "Motley",
+    position: "FB" ,
+    seasons: 9,
+    years: "1946-1953",
+    stats:"4,720 rushing yards /n 5.7 yards per carry",
+    bio:"'I was as big as the linemen I ran against, so I didn't worry about them. And once I ran over a back twice, I didn't have to run over him a third time. He had reservations by then.'"
+
+  },
+  {
+    img: "images/ottoGraham.png",
+    firstName: "Otto",
+    nickname: "",
+    lastname: "Graham",
+    position: "QB" ,
+    seasons: 10,
+    years: "1946-55",
+    stats:"23,584 passing yards /n 174 TDs /n 44 rushing TDs",
+    bio: "'Pro football is no game for weaklings - everyone knows that. The men are rugged, they play hard, but they play clean. When men get into big time football, they're good enough not to have to play dirty. We love the game and we believe football competition, as well as sports competition, makes high types of men.' "
+
+  },
+  {
+    img: "images/ozzieNewsome.png",
+    firstName: "Ozzie",
+    nickname: "'Wizard of Oz'",
+    lastname: "Newsome",
+    position: "TE" ,
+    seasons: 13,
+    years: "1978-90",
+    stats:"662 career receptions /n 7,980 receiving yards /n 47 TDs /n 150 consecutive games with a reception",
+    bio:"'I don't like dropping anything. I haven't played this game as long as I have, as well as I have, dropping footballs... I guess I'm a perfectionist.'"
+
+  },
+  {
+    img: "images/paulWarfield.png",
+    firstName: "Paul",
+    nickname: "",
+    lastname: "Warfield",
+    position: "WR" ,
+    seasons: 13,
+    years: "1964-69, 1976-77",
+    stats:"426 receptions /n 8,565 yards /n 85 TDs",
+    bio: "'What the zone defense has done is take away all the artistry out of pass receiving. It's like taking brushes from an artist.'"
+
+  },
+
+];
+
+export const renderPlayerCard = function(player) {
+    let string = `
+    <img width="20%" height="20%" src="${player.img}"></img>
+ <h3 style="color: black"> ${player.firstName} ${player.nickname} ${player.lastname}</h3>
+ <h4 style="color: black"> ${player.position}</h4>
+<h5 style="color: black"> ${player.seasons} seasons (${player.years})</h5>
+<h5 style="color: black"> ${player.bio}</h5>
+<br>
+ 
+ `;
+return string;
+};
+
+
+export const loadPlayersIntoDOM = function(players) {
+    const $root = $('#root');
+    players.forEach(player => {
+        $root.append(renderPlayerCard(player));
+
+    });
+
+}
+
+$(function() {
+    loadPlayersIntoDOM(hallOfFamers);
+    console.log(hallOfFamers);
+})
